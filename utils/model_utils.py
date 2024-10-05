e3f31cc30365ed6bbabcd6a61bb1f51023aa3163
HTHloveYDH <100819491+HTHloveYDH@users.noreply.github.com>
2024-10-05 23:03:10 +0800

    Update model_utils.py

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -7,13 +7,8 @@ from collections import defaultdict, OrderedDict
 
 import torch
 import torch.nn as nn
-from torch.distributed.algorithms._checkpoint.checkpoint_wrapper import (
-    checkpoint_wrapper as ptd_checkpoint_wrapper,
-)
 
-from dist.ParallelArgs import ParallelArgs
 from models.ModelArgs import ModelArgs
-from utils.logging import logger
 
 
 def get_num_params(model:nn.Module, exclude_embedding:bool=False) -> int:
@@ -36,96 +31,7 @@ def get_num_flop_per_token(num_params:int, model_args:ModelArgs) -> int:
     # 3. each matmul performs 1 multiplication and 1 addition                 (*2)
     # 4. we follow the convention and do not account for sparsity in causal attention
     flop_per_token = 6 * num_params + 12 * l * h * q * t
-
     return flop_per_token
-
-def enable_compile(model:nn.Module):
-    """
-    Apply torch.compile to each TransformerBlock, which makes compilation efficient due to
-    repeated structure. Alternatively one can compile the whole model (after applying DP).
-    """
-    for layer_id, transformer_block in model.layers.named_children():
-        transformer_block = torch.compile(transformer_block, fullgraph=True)
-        model.layers.register_module(layer_id, transformer_block)
-
-    logger.info("Compiling each TransformerBlock with torch.compile")
-
-# for selective op activation checkpointing
-_save_list = {
-    torch.ops.aten.mm.default,
-    torch.ops.aten._scaled_dot_product_efficient_attention.default,
-    torch.ops.aten._scaled_dot_product_flash_attention.default,
-    torch.ops._c10d_functional.reduce_scatter_tensor.default,
-}
-
-def _enable_ac_to_transformer_block(module:nn.Module, parallel_args:ParallelArgs):
-    valid_ac_modes = ("full", "selective")
-    if parallel_args.mode not in valid_ac_modes:
-        raise ValueError(
-            f"Invalid AC mode: {parallel_args.mode}. Valid modes: {valid_ac_modes}"
-        )
-
-    if parallel_args.mode == "full":
-        return ptd_checkpoint_wrapper(module, preserve_rng_state=False)
-
-    assert parallel_args.mode == "selective", f"{parallel_args.mode}"
-    use_op_sac = parallel_args.selective_ac_option == "op"
-    use_layer_sac = parallel_args.selective_ac_option.isdigit()
-    if not use_op_sac and not use_layer_sac:
-        raise ValueError(
-            f"Invalid selective AC option: {ac_config.selective_ac_option}. "
-            f"Valid options: 'op' or a positive int representing layer frequency"
-        )
-    if use_op_sac:
-        from torch.utils.checkpoint import (
-            CheckpointPolicy,
-            create_selective_checkpoint_contexts,
-        )
-
-        def _get_custom_policy(meta):
-            def _custom_policy(ctx, func, *args, **kwargs):
-                mode = "recompute" if ctx.is_recompute else "forward"
-                mm_count_key = f"{mode}_mm_count"
-                if func == torch.ops.aten.mm.default:
-                    meta[mm_count_key] += 1
-                # Saves output of all compute ops, except every second mm
-                to_save = func in _save_list and not (
-                    func == torch.ops.aten.mm.default and meta[mm_count_key] % 2 == 0
-                )
-                return (
-                    CheckpointPolicy.MUST_SAVE
-                    if to_save
-                    else CheckpointPolicy.PREFER_RECOMPUTE
-                )
-
-            return _custom_policy
-
-        def selective_checkpointing_context_fn():
-            meta = defaultdict(int)
-            return create_selective_checkpoint_contexts(_get_custom_policy(meta))
-
-        return ptd_checkpoint_wrapper(
-            module,
-            context_fn=selective_checkpointing_context_fn,
-            preserve_rng_state=False,
-        )
-    elif use_layer_sac:
-        # Checkpoint every `ac_freq` of the modules passed to this function
-        ac_freq = int(parallel_args.selective_ac_option)
-        ptd_checkpoint_wrapper.__dict__.setdefault("_count", 0)
-        ptd_checkpoint_wrapper._count += 1
-        if not ac_freq or ptd_checkpoint_wrapper._count % ac_freq == 0:
-            return ptd_checkpoint_wrapper(module, preserve_rng_state=False)
-        else:
-            return module
-
-def enable_activation_checkpoint(model:nn.Module, parallel_args:ParallelArgs):
-    """Apply activation checkpointing to the model."""
-    for layer_id, transformer_block in model.layers.named_children():
-        transformer_block = _enable_ac_to_transformer_block(transformer_block, parallel_args)
-        model.layers.register_module(layer_id, transformer_block)
-
-    logger.info(f"Applied {mode} activation checkpointing to the model")
 
 def convert(ckpt_path:str, format:str, save_dir:str, splits=3):
     import json
