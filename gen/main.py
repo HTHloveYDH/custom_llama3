7095531502520c457b3c2cef00791e08388670bb
HTHloveYDH <100819491+HTHloveYDH@users.noreply.github.com>
2024-10-07 00:58:01 +0800

    Update main.py

--- gen/main.py
+++ gen/main.py
@@ -55,8 +55,8 @@ def main():
     torch.set_float32_matmul_precision('high')
 
     ''' ____________________________________ build & compile model ___________________________________ '''
-    model, _, _ = get_model(llama_config, device)
-    pp_schedule = parallelize_model(model, parallel_args, device_mesh, False)
+    model, _ = get_model(llama_config, device)
+    _, _ = parallelize_model(model, parallel_args, device_mesh, False)
 
     ''' ____________________________________________ test ___________________________________________ '''
     # _, _ = generate(model, prompt, gen_batch_size, gen_len, temperature, top_p, device=device)
