680178c14d7c590f6c78cd9e6c9c471dc16b1808
HTHloveYDH <hanteikou@gmail.com>
2024-10-07 23:36:03 +0800

    update 0925

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,7 +1,9 @@
 import os
+import json
 from collections import OrderedDict
 
 import torch
+import torch.nn as nn
 
 
 def convert(ckpt_path:str, format:str, save_dir:str, splits=3):
@@ -23,14 +25,25 @@ def convert(ckpt_path:str, format:str, save_dir:str, splits=3):
 
     def _convert_to_safetensors(model:nn.Module, save_dir:str, splits:int):
         from safetensors.torch import save_file
+        json_content = {'metadata': {'total_size': None}, 'weight_map': {}}
         state_dict = model.state_dict()
         keys = list(state_dict.keys())
         T = len(keys) // splits
+        # for first N - 1 splits
         for i in range(splits - 1):
             state_dict_split = OrderedDict()
             for j in range(i * T, (i + 1) * T):
+                json_content['weight_map'][keys[j]] = f'model_split_{i + 1}_of_{splits}.safetensors'
                 state_dict_split[keys[j]] = state_dict[keys[j]]
-            save_file(state_dict_split, os.path.join(save_dir, f'./model_split_{i}.safetensors'))
+            save_file(state_dict_split, os.path.join(save_dir, f'./model_split_{i + 1}_of_{splits}.safetensors'))
+        # for last split
+        for j in range((splits - 1) * T, len(keys)):
+            json_content['weight_map'][keys[j]] = f'model_split_{splits}_of_{splits}.safetensors'
+            state_dict_split[keys[j]] = state_dict[keys[j]]
+        save_file(state_dict_split, os.path.join(save_dir, f'./model_split_{splits}_of_{splits}.safetensors'))
+        # create model.safetensors.index.json
+        with open(os.path.join(save_dir, 'model.safetensors.index.json'), 'w') as f:
+            f.
 
     with open('./config/llama_config.json', 'r') as f:
         llama_config = json.load(f)
@@ -42,6 +55,6 @@ def convert(ckpt_path:str, format:str, save_dir:str, splits=3):
     llama = Transformer.from_local_pretrained(llama_config)
     llama.load_state_dict(state_dict)
     if format == 'onnx':
-        _convert_to_onnx(llama)
+        _convert_to_onnx(llama, save_dir)
     elif format == 'safetensors':
-        _convert_to_safetensors(llama, splits)
+        _convert_to_safetensors(llama, save_dir, splits)
